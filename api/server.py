7162de79ff6799b9cc0add4dcc2b15cc6856d8b9
Tom <tomcremoi@gmail.com>
2025-10-01 16:26:13 +0200

    ajout structure CRUD basique

--- api/server.py
+++ api/server.py
@@ -1,9 +1,22 @@
-import flask
+from flask import Flask
 
+app = Flask(__name__)
 
-app = flask.Flask(__name__)
-@app.route('/')
-
-
+@app.route("/")
 def index():
-    return "oui"
+
+@app.route("/add", methods=["POST"])
+def add_item():
+    return
+
+@app.route("/update/<int:id>", methods=["POST"])
+def update_item(id):
+    return
+
+@app.route("/delete/<int:id>")
+def delete_item(id):
+    return
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
